c46d484f65d9fd2775a9af2d492254be682962db
Ishini Hathurusinghe <101358580+ishinihathurusinghe@users.noreply.github.com>
2022-06-23 17:29:15 +0530

    Add files via upload

---
+++ methods.php
@@ -0,0 +1,18 @@
+<?php
+
+class Employee{
+
+     function greet(){
+
+        return 'Ayubovan';
+     }
+
+}
+
+$employee_1 = new Employee;
+
+echo $employee_1->greet();
+
+
+
+?>
---
+++ methods3.php
@@ -0,0 +1,29 @@
+<?php
+
+class Employee{
+
+     function greet(){
+
+        return 'Ayubovan';
+     }
+
+}
+
+$employee_1 = new Employee;
+
+echo $employee_1->greet();
+
+$class_method = get_class_methods('Employee');
+
+echo '<pre>';
+print_r($class_method);
+echo '</pre>';
+
+
+if(method_exists('Employee','greet')){
+    echo 'method greet exists';
+}else{
+    echo 'method greet does not exists'; 
+}
+
+?>
